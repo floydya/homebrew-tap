5d1e7d9204bd992263539d55544fba7e61dc0c60
Vladyslav Halatskyi <10259080+floydya@users.noreply.github.com>
2026-09-23 14:32:58 +0300

    chore(VLT-REL): update vlt to 0.2.2 (#6)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/vlt.rb
+++ Formula/vlt.rb
@@ -1,8 +1,8 @@
 class Vlt < Formula
   desc "Profile manager and transparent launcher for HashiCorp Vault"
   homepage "https://github.com/floydya/vlt"
-  url "https://github.com/floydya/vlt/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "f303186c7ad192a28989062d385bc49c807685114784c3c764c14663360b6bd6"
+  url "https://github.com/floydya/vlt/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "b86e30c3f2091c478ab28f6d76f3195f41ebb527a9d7d1b8fbf6fbd683cf1c54"
   license "MIT"
 
   depends_on "go" => :build
